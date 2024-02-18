9bff1aee8d8d1350264b7632d74af9dc8055b935
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-02-18 13:57:09 -0500

    Produce model.

---
+++ models/produce.go
@@ -0,0 +1,7 @@
+package models
+
+type Produce struct {
+	Code  string  `json:"code"`
+	Name  string  `json:"name"`
+	Price float32 `json:"price"`
+}
--- produce-store.go
+++ produce-store.go
@@ -8,22 +8,29 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.POST("/produce", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.GET("/produce", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
-	r.GET("/search", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/produce", PostProduce)
+	r.GET("/produce/:id", GetProduce)
+	r.GET("/search", SearchProduce)
 	if err := r.Run(); err != nil {
 		slog.Error("%s", err)
 	}
+}
+
+func GetProduce(c *gin.Context) {
+	id := c.Param("id")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong" + id,
+	})
+}
+
+func SearchProduce(c *gin.Context) {
+	name := c.Query("name")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong" + name,
+	})
+}
+
+func PostProduce(c *gin.Context) {
 }

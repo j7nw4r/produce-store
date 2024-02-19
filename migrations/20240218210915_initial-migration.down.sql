aa5ffc12e76bc3d6e8285cf9d0dc5a0a2c57e0f1
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-02-18 21:07:19 -0500

    test db working

---
+++ migrations/20240218210915_initial-migration.down.sql
@@ -0,0 +1,2 @@
+drop index produce_name_idx;
+drop table produce;
--- migrations/20240218210915_initial-migration.up.sql
+++ migrations/20240218210915_initial-migration.up.sql
@@ -1,4 +1,12 @@
-create table if not exists main.produce (
-    id serial primary key,
-    eid UUID
-)
+create table if not exists produce (
+    id integer primary key,
+    code text,
+    name text,
+    price float
+);
+create index produce_name_idx on produce (name);
+
+insert into produce (code, name, price) values ('A12T-4GH7-QPL9-3N4M', 'lettuce', 3.46);
+insert into produce (code, name, price) values ('E5T6-9UI3-TH15-QR88', 'peach', 2.99);
+insert into produce (code, name, price) values ('YRT6-72AS-K736-L4AR', 'green pepper', 0.79);
+insert into produce (code, name, price) values ('TQ4C-VV6T-75ZX-1RMR', 'gala apple', 3.59);
